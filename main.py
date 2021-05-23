ec534bf80560c61804b011938f7dd10c2858250b
DavidRich09 <luis.richmond9@gmail.com>
2021-05-23 03:10:00 -0600

    combbox y treeview con tkk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 import os
 
 #FUNCIÓN PARA CARGAR IMAGEN
@@ -52,6 +53,17 @@ def ImprimirNombre(canvas, text1,textoNombre):
 
 def Ventana2(canvas):
 
+    def ActualizarItem():
+
+        #OBTENER SELECCIONADO
+        seleccionado = VistaLista.focus()
+        #CAMBIAR VALORES
+        VistaLista.item(seleccionado,values = ['0', '0', 'El cielo','100'])
+
+        #OBTENER VALORES DEL SELECCIONADO
+        temporal = VistaLista.item(seleccionado,"values")
+        print(temporal)
+
     #REDEFINICIÓN CANVAS
     canvas = Canvas(ventana, width=500, height=500, bg="blue")
     canvas.place(x=0, y=0)
@@ -60,11 +72,92 @@ def Ventana2(canvas):
     fondo = CargarImagen("fondo2.png")
     canvas.create_image(0,0,image = fondo, anchor = NW)
 
+    #Listas en tree view
+    VistaLista = ttk.Treeview(canvas)
+    VistaLista["columns"] = ("País","Hotel","Nombre","Cantidad Estrellas")
+
+    #CREAR COLUMNAS
+    VistaLista.column('#0', width=0, stretch=NO)
+    VistaLista.column("País", anchor = CENTER, width = 80)
+    VistaLista.column("Hotel", anchor=CENTER, width=80)
+    VistaLista.column("Nombre", anchor=CENTER, width=80)
+    VistaLista.column("Cantidad Estrellas", anchor=CENTER, width=80)
+
+    #CONFIGUAR COLUMAAS
+    VistaLista.heading('#0', text='', anchor=CENTER)
+    VistaLista.heading('País', text='País', anchor=CENTER)
+    VistaLista.heading('Hotel', text='Hotel', anchor=CENTER)
+    VistaLista.heading('Nombre', text='Nombre', anchor=CENTER)
+    VistaLista.heading('Cantidad Estrellas', text='CE', anchor=CENTER)
+
+    #INSERTAR FILAS
+    VistaLista.insert(parent='', index=0, iid=0, text='', values=['1', '1', 'Panchita','5'])
+    VistaLista.insert(parent='', index=1, iid=1, text='', values=['1', '2', 'Rio', '5'])
+    VistaLista.insert(parent='', index=2, iid=2, text='', values=['3', '3', 'Tamarindo', '5'])
+    VistaLista.insert(parent='', index=3, iid=3, text='', values=['14', '4', 'Lonche', '5'])
+    VistaLista.insert(parent='', index=4, iid=4, text='', values=['1', '6', 'TEC residencia', '-5'])
+
+
+    # INSERTAR AUTOMATICAMENTE
+    listaAInsertar = [['1', '1', 'Panchita','5'],['1', '1', 'Panchita','5'],['1', '1', 'Panchita','5'],['1', '1', 'Panchita','5']] #lista a insertar
+
+    index = 5 #indice de fila, en este caso empieza en 5 por los cienco elementos anteriores, generalmente empieza en 0
+    for i in listaAInsertar: #for para recorrer la lista que queremos insertar
+        VistaLista.insert(parent='', index=index, iid=index, text='', values=i)
+        index+=1 # aumentamos indice
+
+    VistaLista.place(x=90,y=50)
+
+    cambiar = Button(canvas,text = "Actualizar",command = ActualizarItem)
+    cambiar.place(x=300,y=420)
+
+
     imagenB = CargarImagen("Batrs.png")
     atras = Button(canvas,text = "Atrás",image = imagenB,command = VentanaPrincipal)
     atras.place(x=400,y=420)
 
+    siguiente = Button(canvas,text = "Siguiente",command = lambda : Ventana4(canvas))
+    siguiente.place(x=200,y=420)
+
     ventana.mainloop()
+
+
+def Ventana4(canvas):
+
+    def ObtenerItem():
+
+        nombre = listaDesplegable.get()
+        indice = listaDesplegable.current()
+
+        print(nombre,indice)
+
+    #REDEFINICIÓN CANVAS
+    canvas = Canvas(ventana, width=500, height=500, bg="blue")
+    canvas.place(x=0, y=0)
+
+    #CARGAR IMAGEN Y PONERLA DE FONDO
+    fondo = CargarImagen("fondo2.png")
+    canvas.create_image(0,0,image = fondo, anchor = NW)
+
+    #LISTA DESPLEGABLE
+
+    opciones = ["teletica","movistar","kolbi","pasar intro"] #lista de opciones
+
+    listaDesplegable = ttk.Combobox(canvas,width=20,state = "reandoly") #genero lista desplegable con combbox
+    listaDesplegable.place(x=100,y=100)
+
+    listaDesplegable["values"] = opciones #Lo doy los valores
+    listaDesplegable.set("Elige una opcion") #eligo el estado inicial
+
+    cambiar = Button(canvas,text = "Obtener",command = ObtenerItem)
+    cambiar.place(x=300,y=420)
+
+
+    ventana.mainloop()
+
+
+
+
 
 def Ventana3():
 
