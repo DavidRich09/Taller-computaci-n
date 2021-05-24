cdced5f509c2340129cab868a3f123402fad8447
PabloNaranjo78 <67924944+PabloNaranjo78@users.noreply.github.com>
2021-05-23 21:25:07 -0600

    Se edita lo de json

--- JSON/JsonManager.py
+++ JSON/JsonManager.py
@@ -4,13 +4,14 @@ def dicToJson(string):
     jsonData = json.loads(string)
     return jsonData
 
-
 def JsonToString(jsonData):
     string = json.dumps(jsonData)
     return string
 
+jsonDataIn =dicToJson('{ "name":"John", "age":30, "city":"New York"}')
+print(jsonDataIn)
+print(jsonDataIn["city"])
 
-print(dicToJson('{ "name":"John", "age":30, "city":"New York"}'))
 print(type((dicToJson('{ "name":"John", "age":30, "city":"New York"}'))))
 
 print(JsonToString({'name': 'John', 'age': 30, 'city': 'New York'}))
